ffedc3e8ce2f5ebe6084b1659d294911709b7bc8
Jose Cabarcas <jose@elenas.la>
2021-04-06 17:41:32 -0500

    [Fix] Show messaging type

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,9 @@ declare module 'zendesk-sdk-react-native' {
   // normal init function when you want to use all of the sdks
   export function init(initializationOptins: InitOptions, callback: CallbackFunction): void;
 
+  // function to show the conversation screen.
+  export function showMessaging(): void;
+
   export interface InitOptions {
     // chat key of zendesk account to init chat
     key: string,
